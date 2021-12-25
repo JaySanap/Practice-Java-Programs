e1c820e4c0450a287b7d2def1fbaa633a5ab8f7e
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2021-12-25 20:55:18 +0530

    Add files via upload

---
+++ STARPATTERN6.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class STARPATTERN6 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print(" *");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
