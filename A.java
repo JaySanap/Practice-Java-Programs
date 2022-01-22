7bc66057b28978f6395b04b44802e5ed0c3c0866
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2022-01-22 21:01:54 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class A {
+    int a=10;
+}
+class B extends A {
+    int a = 20;
+
+    void show(int a) {
+        System.out.println(super.a);
+    }
+
+    public static void main(String[] args) {
+        B obj1=new B();
+        obj1.show(30);
+    }
+}
