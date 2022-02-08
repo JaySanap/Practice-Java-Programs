6818118fb08141b73b8c92e6195ca8233443487d
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2022-02-08 11:24:14 +0530

    Add files via upload

---
+++ Wrapperclass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Wrapperclass {
+    public static void main(String[] args) {
+        int i=100;
+        Integer iobj=Integer.valueOf(i);
+        Integer iobj1=i;
+        System.out.print(iobj+iobj1);
+    }
+}
